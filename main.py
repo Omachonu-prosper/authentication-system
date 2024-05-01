5455f136567e82883e1dad2b8c3c86299de759d7
Omachonu Prosper <omachonucodes@gmail.com>
2024-05-02 00:04:17 +0100

    Add doc text for endpoint description in api docs

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ app = FastAPI()
 
 @app.get('/', status_code=status.HTTP_200_OK)
 async def root():
+    """The root endpoint"""
+    
     return {
         'message': 'Authentication module v1',
         'status': True
     }
 
 
-@app.post('/signup', status_code=status.HTTP_201_CREATED)
+@app.post('/auth/signup', status_code=status.HTTP_201_CREATED)
 async def signup():
+    """Create a new user"""
     return {
         'message': 'New user signup successful',
         'status': True
     }
 
 
-@app.post('/login', status_code=status.HTTP_200_OK)
+@app.post('/auth/login', status_code=status.HTTP_200_OK)
 async def login():
+    """Login with the credentials of an already signed up user"""
     return {
         'message': 'User login successful',
         'status': True
@@ -28,8 +32,11 @@ async def login():
 
 
 @app.get('/protected', status_code=status.HTTP_200_OK)
-async def protected():
+async def protected(qparam: str = ""):
+    """Protected route only accessible to loged in users"""
+    
     return {
         'message': 'User authorized to access protected route',
-        'status': True
+        'status': True,
+        'query-param': qparam
     }
