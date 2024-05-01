eb1f6c66ca7137339a66864c17ecebac9026791c
Omachonu Prosper <omachonucodes@gmail.com>
2024-05-02 00:46:50 +0100

    Handle exceptions and response format

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI, status
+import os
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class BasicResponse(BaseModel):
+    message: str
+    status: bool
+    data: list[dict] | dict[str, str] = None
+
 
 app = FastAPI()
+client = AsyncIOMotorClient(os.getenv('MONGO_URI'))
+db = client.AuthModule
 
 
 @app.get('/', status_code=status.HTTP_200_OK)
-async def root():
+async def root() -> BasicResponse:
     """The root endpoint"""
     
     return {
@@ -14,8 +32,23 @@ async def root():
 
 
 @app.post('/auth/signup', status_code=status.HTTP_201_CREATED)
-async def signup():
+async def signup(user: User) -> BasicResponse:
     """Create a new user"""
+    username_taken = await db.Users.find_one({'username': user.username})
+    if username_taken:
+        raise HTTPException(
+            status_code=409,
+            detail='The username is taken'
+        )
+    
+    email_taken = await db.Users.find_one({'email': user.email})
+    if email_taken:
+        raise HTTPException(
+            status_code=409,
+            detail='The email is registered to another user'
+        )
+    
+    await db.Users.insert_one(dict(user))
     return {
         'message': 'New user signup successful',
         'status': True
@@ -23,7 +56,7 @@ async def signup():
 
 
 @app.post('/auth/login', status_code=status.HTTP_200_OK)
-async def login():
+async def login() -> BasicResponse:
     """Login with the credentials of an already signed up user"""
     return {
         'message': 'User login successful',
@@ -32,7 +65,7 @@ async def login():
 
 
 @app.get('/protected', status_code=status.HTTP_200_OK)
-async def protected(qparam: str = ""):
+async def protected(qparam: str = "") -> BasicResponse:
     """Protected route only accessible to loged in users"""
     
     return {
