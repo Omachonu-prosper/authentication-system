859a73fc11002aa6c45beca6599699223fece3b7
Omachonu Prosper <omachonucodes@gmail.com>
2024-05-01 13:07:13 +0100

    Write the api endpoint boilerplate

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def root():
     return {
         'message': 'Authentication module v1',
+        'status': True
+    }
+
+
+@app.post('/signup', status_code=status.HTTP_201_CREATED)
+async def signup():
+    return {
+        'message': 'New user signup successful',
+        'status': True
+    }
+
+
+@app.post('/login', status_code=status.HTTP_200_OK)
+async def login():
+    return {
+        'message': 'User login successful',
+        'status': True
+    }
+
+
+@app.get('/protected', status_code=status.HTTP_200_OK)
+async def protected():
+    return {
+        'message': 'User authorized to access protected route',
         'status': True
     }
